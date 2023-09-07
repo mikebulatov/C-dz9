836ea71d56a7270d25b5eaad7657acdde9756211
Mikebulatov <mikebulatov@yandex.ru>
2023-09-07 22:24:38 +0700

    Задача 64. Изменение.

--- zd64/Program.cs
+++ zd64/Program.cs
@@ -1,52 +1,14 @@
-﻿Console.WriteLine("Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+﻿Console.WriteLine("Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.");
 
-
-Console.Write("Введите первую размерность массива:");
-int i=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите вторую размерность массива:");
-int j=Convert.ToInt32(Console.ReadLine());
-
-int [,] matrix=new int [i,j];
-//формируем массиы
-Console.WriteLine("Сформированный массив:");
-for(i=0; i<matrix.GetLength(0); i++)
+void PrintNum(int start)
 {
-    for(j=0; j<matrix.GetLength(1); j++)
-    {
-    matrix[i,j]= new Random().Next(0,10);
-     Console.Write("{0:D2} ", matrix[i,j]);
-    
-    }
-    Console.WriteLine();
+  if(start>0)
+  {
+  Console.Write("{0:D} ", start);
+  PrintNum(start-1);
+  } 
 }
 
-//сортируем строки массива
-for(i=0; i<matrix.GetLength(0); i++)
-{
-  
-    for(j=0; j<matrix.GetLength(1); j++)
-    {
-      for(int k=j+1; k<matrix.GetLength(1); k++)
-      if(matrix[i,k]>matrix[i,j])
-      {
-       matrix[i,j]=matrix[i,j]+matrix[i,k];
-       matrix[i,k]=matrix[i,j]-matrix[i,k];
-       matrix[i,j]=matrix[i,j]-matrix[i,k];
-      }
-    
-    
-    }
-   
-}
-//Выводим упорядоченный массив
-Console.WriteLine("Упорядоченный массив:");
-for(i=0; i<matrix.GetLength(0); i++)
-{
-  for(j=0; j<matrix.GetLength(1); j++)
-    {
-     Console.Write("{0:D2} ", matrix[i,j]);
-    }
-    Console.WriteLine();
-}
-
+Console.Write("Введите число N:");
+PrintNum(Convert.ToInt32(Console.ReadLine()));
 Console.WriteLine();
